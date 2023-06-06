f3fe783c63916c9c664ff2d6ad4984ff6b77e767
Smruti Ranjan Nayak <56423859+SM8UTI@users.noreply.github.com>
2023-06-06 14:36:58 +0530

    Register page created and style �

---
+++ src/Pages/Register.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import logo from "../Assets/LogoName.svg";
+import profile from "../Assets/profile.svg";
+import { Link } from "react-router-dom";
+
+const Register = () => {
+  return (
+    <div className="Register min-h-screen grid content-center  bg-lightPrimary p-4">
+      <div className="container flex items-center flex-col gap-4 bg-white mx-auto p-8 rounded-lg w-full sm:max-w-[450px] ">
+        <div className="logo ">
+          <img
+            src={logo}
+            alt="NMIETBOOK"
+            className="w-[180px] object-contain"
+          />
+        </div>
+        <div className="text flex flex-col items-center gap-1 text-center">
+          <h1 className="text-3xl font-semibold">Create Your Account</h1>
+          <p className="text-base opacity-75">😃 Sign up and 💬 Chat </p>
+        </div>
+        <form className="flex flex-col gap-4 w-full">
+          <div className="flex flex-col gap-2">
+            <label htmlFor="userName" className="text-sm opacity-80">
+              User Name
+            </label>
+            <input
+              type="text"
+              id="userName"
+              placeholder="John123"
+              className="border-2 border-slate-500 p-2 rounded-md text-sm text-black  w-full"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="email" className="text-sm opacity-80">
+              Email Address
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="John123@gmail.com"
+              className="border-2 border-slate-500 p-2 rounded-md text-sm text-black w-full "
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="password" className="text-sm opacity-80">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              placeholder="*****"
+              className="border-2 border-slate-500 p-2 rounded-md text-sm text-black w-full "
+            />
+          </div>
+          <div className="max-w-fit ">
+            <label
+              htmlFor="image"
+              className="flex flex-row items-center gap-2 cursor-pointer"
+            >
+              <img
+                src={profile}
+                alt="profile choose"
+                className="w-[60px] aspect-square object-contain"
+              />
+              <span className="opacity-80">Add an Avatar</span>
+            </label>
+            <input type="file" id="image" className="hidden" />
+          </div>
+          <button
+            type="submit"
+            className="bg-primary text-white py-3 rounded-md"
+          >
+            Register
+          </button>
+        </form>
+        <div>
+          <h4 className="font-normal text-sm">
+            Don't have any account?
+            <Link to={"/login"} className="text-primary font-medium underline ml-1">
+              Login
+            </Link>
+          </h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./Pages/Login.jsx";
+import Register from "./Pages/Register.jsx";
+
+const AppRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={AppRouter} />
+  </React.StrictMode>
+);
